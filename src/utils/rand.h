ac307a5fbfa95f46208fcfdab188010755df1ea3
坤 徐 <me@biduang.cn>
2023-08-28 09:56:05 +0800

    rebuild: 弃用 rand.h，改为使用 random 实现真随机函数 chore: 添加 .gitignore

---
+++ src/utils/rand.h
@@ -0,0 +1,12 @@
+//
+// Created by BiDuang on 2023/8/28.
+//
+
+#ifndef GAMETOWER_RAND_H
+#define GAMETOWER_RAND_H
+
+#include <random>
+
+extern int randInt(int min, int max);
+
+#endif //GAMETOWER_RAND_H
