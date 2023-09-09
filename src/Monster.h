3739df756a5505fbe38e69b71ec5c2693704915b
Skyecrane <69761834+SkyeMoonchy@users.noreply.github.com>
2023-09-10 03:41:10 +0800

    怪物部分完成

--- src/Monster.h
+++ src/Monster.h
@@ -8,49 +8,42 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unistd.h>
 #include "Creature.h"
 #include "Word.h"
 
 class Monster : public Creature {
 public:
     Monster() = default;
+    Monster(int monsterNumber);
     Monster(int HP, int damage, std::vector<Word> wordList, const std::string description);
     std::vector<Word> deliverWord() { return this->wordList; }
     int getHP() { return hp; }
     std::string des() { return this->description; }
+    void readMonsterData(int monsterNumber);
 private:
     int damage;
     std::vector<Word> wordList;
     std::string description;
 };
 
-Monster::Monster(int HP, int damage, std::vector<Word> wordList, const std::string description) {
-    this->damage = damage;
-    this->wordList = wordList;
-    this->hp = HP;
-    this->description = description;
-    this->curhp=hp;
+Monster::Monster(int monsterNumber) {
+    readMonsterData(monsterNumber);
 }
 
-std::vector<std::string> readMonsterDes(const std::string& filename) {
-    std::ifstream file(filename);
-    std::vector<std::string> descriptions;
-    std::string line;
+void Monster::readMonsterData(int monsterNumber) {
+    std::string numberString = std::to_string(monsterNumber);
+    std::string filePath = "./Assets/.Monster" + numberString;
+    std::ifstream file(filePath);
+    std::string word;
 
-    while (std::getline(file, line)) {
-        descriptions.push_back(line);
-    }
+    file >> hp;
+    file >> damage;
+    curhp = hp;
+    std::getline(file >> std::ws, description);
 
-    return descriptions;
+    while (file >> word)
+        wordList.push_back(Word(word.size(), word, 'd'));
+
 }
-
-//Monster createMonster(int hp, int damage, std::string description) {
-//    std::vector<Word> wordList;
-//    Monster monster(hp, damage, wordList, description);
-//    // TODO: WordList
-//
-//    return monster;
-//}
-
-
 #endif //GAMETOWER1_MONSTER_H
